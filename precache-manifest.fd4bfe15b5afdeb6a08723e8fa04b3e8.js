5119c027d8248a60d5c5cba7bf69a06c3a50f15e
tilhy349 <57438314+tilhy349@users.noreply.github.com>
2019-12-12 15:26:28 +0100

    Add files via upload

---
+++ precache-manifest.fd4bfe15b5afdeb6a08723e8fa04b3e8.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54c62553629714bff1eb5365974c472d",
+    "url": "/ckfinal/index.html"
+  },
+  {
+    "revision": "635067def1b63ccf30ca",
+    "url": "/ckfinal/static/css/main.df36984f.chunk.css"
+  },
+  {
+    "revision": "f394824782cba9006058",
+    "url": "/ckfinal/static/js/2.8346c4af.chunk.js"
+  },
+  {
+    "revision": "783efe56c297789f679b639c509e9a30",
+    "url": "/ckfinal/static/js/2.8346c4af.chunk.js.LICENSE"
+  },
+  {
+    "revision": "635067def1b63ccf30ca",
+    "url": "/ckfinal/static/js/main.e4e6e883.chunk.js"
+  },
+  {
+    "revision": "160c18376378366becd0",
+    "url": "/ckfinal/static/js/runtime-main.c1ae53b4.js"
+  },
+  {
+    "revision": "6ef0a25e7801e0b83d4d5079d61ed942",
+    "url": "/ckfinal/static/media/K_1.6ef0a25e.svg"
+  },
+  {
+    "revision": "6ef0a25e7801e0b83d4d5079d61ed942",
+    "url": "/ckfinal/static/media/K_2.6ef0a25e.svg"
+  },
+  {
+    "revision": "1f650f0907d862c2808b58a00162dc31",
+    "url": "/ckfinal/static/media/K_3.1f650f09.svg"
+  },
+  {
+    "revision": "88cd500108e69339a1f832f26c585af0",
+    "url": "/ckfinal/static/media/K_4.88cd5001.svg"
+  },
+  {
+    "revision": "56101dc678fc8011108b0fbf746cf670",
+    "url": "/ckfinal/static/media/K_5.56101dc6.svg"
+  },
+  {
+    "revision": "1a16c776f2fb157f7a34772e833cc33b",
+    "url": "/ckfinal/static/media/TP51.1a16c776.png"
+  },
+  {
+    "revision": "c476346d060f40fd413d44d7ee729239",
+    "url": "/ckfinal/static/media/TP_3.c476346d.svg"
+  },
+  {
+    "revision": "5b917af41b2bb043895c00ed3e843543",
+    "url": "/ckfinal/static/media/TP_4.5b917af4.svg"
+  },
+  {
+    "revision": "56101dc678fc8011108b0fbf746cf670",
+    "url": "/ckfinal/static/media/TP_5.56101dc6.svg"
+  },
+  {
+    "revision": "8e70bcaa5e28b11ba8f9ddedc4c9ca8c",
+    "url": "/ckfinal/static/media/down_arrow.8e70bcaa.png"
+  },
+  {
+    "revision": "5e3c74b28436542416cbb774dbd26724",
+    "url": "/ckfinal/static/media/down_grey.5e3c74b2.svg"
+  },
+  {
+    "revision": "0e73c39a4285f147f036c0908a822dee",
+    "url": "/ckfinal/static/media/down_white.0e73c39a.svg"
+  },
+  {
+    "revision": "a492a15a11cf4415282fa08ee08c8616",
+    "url": "/ckfinal/static/media/help.a492a15a.svg"
+  },
+  {
+    "revision": "d640f19df1cd89bb766db4203a06604c",
+    "url": "/ckfinal/static/media/home.d640f19d.svg"
+  },
+  {
+    "revision": "6aac34646f878138c05d80f1794e1c5e",
+    "url": "/ckfinal/static/media/loggo.6aac3464.svg"
+  },
+  {
+    "revision": "27d139ff57744bb448d4d7251a147207",
+    "url": "/ckfinal/static/media/ny_pin.27d139ff.svg"
+  },
+  {
+    "revision": "3776fc90c0f6385eb05d3e30f67d4259",
+    "url": "/ckfinal/static/media/overview.3776fc90.jpg"
+  },
+  {
+    "revision": "5938b46f999610a1b99fabf078a2526c",
+    "url": "/ckfinal/static/media/overview.5938b46f.svg"
+  },
+  {
+    "revision": "c5d269d0d6e773f51bb6cb235bf72fd7",
+    "url": "/ckfinal/static/media/up_arrow.c5d269d0.png"
+  },
+  {
+    "revision": "b784f96ac72e0d1b45e7fb455423acf4",
+    "url": "/ckfinal/static/media/up_grey.b784f96a.svg"
+  },
+  {
+    "revision": "2a18943e0cda74545439795b1ae83f42",
+    "url": "/ckfinal/static/media/up_white.2a18943e.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6bcbf49601ff3fc9c3de99040a78dbd3.js"
+  "/ckfinal/precache-manifest.fd4bfe15b5afdeb6a08723e8fa04b3e8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ckfinal/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
